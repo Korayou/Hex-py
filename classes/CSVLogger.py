be5091fc1f1e9d23d16bd1458ef4e1821d762ead
colpiche <114240773+colpiche@users.noreply.github.com>
2024-04-19 13:54:50 +0200

    Added header to CSV

--- classes/CSVLogger.py
+++ classes/CSVLogger.py
@@ -4,9 +4,17 @@ import os
 
 class CSVLogger:
     dataset: np.ndarray
+    header: list[str]
 
     def __init__(self, board_size: int):
         self.dataset = np.zeros(shape=(0, (board_size ** 2) + 2), dtype=np.uint8)
+        self.header = []
+
+        for i in range(board_size ** 2):
+            self.header.append(f"Case_{i}")
+
+        self.header.append("Joueur")
+        self.header.append("Gagnant")
 
     def add_row(self, board: np.ndarray, player: int):
         self.dataset = np.vstack([self.dataset, np.append(board.flatten(), np.array([player, 0]))])
@@ -25,5 +33,5 @@ class CSVLogger:
             os.makedirs(folder_path)
 
         current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        file_path = os.path.join(folder_path, f"dataset_{current_datetime}.csv")
-        np.savetxt(file_path, self.dataset, delimiter=",", fmt="%i")
+        file_path = os.path.join(folder_path, f"game_{current_datetime}.csv")
+        np.savetxt(file_path, self.dataset, delimiter=",", fmt="%i", header=",".join(self.header), comments="")
