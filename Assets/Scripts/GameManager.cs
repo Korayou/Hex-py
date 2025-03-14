6f939396bfda43ec97d06d0957fec5ffef79faf9
TimoMet <timetais22@gmail.com>
2025-03-14 14:24:51 +0100

    generate terrain on runtime

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,16 @@
 
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject hexagonPrefab;
+    [SerializeField] private float hexagonRadius;
+
+    private void Start()
+    {
+        for (var i = -3; i < 4; i++)
+        for (var j = -3; j < 4; j++)
+        {
+            var position = (Vector2.right * j + new Vector2(0.5f, -Mathf.Cos(-Mathf.PI/6)) * i) * hexagonRadius;
+            Instantiate(hexagonPrefab, position, Quaternion.identity, transform);
+        }
+    }
 }
