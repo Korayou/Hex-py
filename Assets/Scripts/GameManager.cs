82a7bd07ad3c54a0fb208e8891ea9cd8250d6fb7
TimoMet <timetais22@gmail.com>
2025-03-14 16:18:48 +0100

    remove unused using

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using NUnit.Framework;
-using Unity.VisualScripting;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
